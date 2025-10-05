bc0b222b52ddc58d39348f9cf14a9e61aa46243e
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-10-06 05:00:37 +0800

    fix: Update modified Vercel lazyvim theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -27,12 +27,6 @@ return {
             "NormalFloat",
             "SignColumn",
             "LineNr",
-            "CursorLine",
-            "CursorLineNr",
-            "EndOfBuffer",
-            "StatusLine",
-            "StatusLineNC",
-            "WinSeparator",
           }
           for _, group in ipairs(groups) do
             vim.api.nvim_set_hl(0, group, { bg = black })
