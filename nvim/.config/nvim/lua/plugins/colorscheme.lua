b8d684bce51e6670d745dcc6feaf051aad77d804
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-10-06 04:51:56 +0800

    fix: Add modified Vercel lazyvim theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -17,6 +17,31 @@ return {
     config = function(_, opts)
       require("vercel").setup(opts)
       vim.cmd.colorscheme("vercel")
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+          local black = "#000000"
+          local groups = {
+            "Normal",
+            "NormalNC",
+            "NormalFloat",
+            "SignColumn",
+            "LineNr",
+            "CursorLine",
+            "CursorLineNr",
+            "EndOfBuffer",
+            "StatusLine",
+            "StatusLineNC",
+            "WinSeparator",
+          }
+          for _, group in ipairs(groups) do
+            vim.api.nvim_set_hl(0, group, { bg = black })
+          end
+        end,
+      })
+      vim.defer_fn(function()
+        vim.cmd("colorscheme vercel")
+      end, 50)
     end,
   },
   {
