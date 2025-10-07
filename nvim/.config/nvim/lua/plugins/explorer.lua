f5999774633a9de05dc6aebafd32e81e52653d66
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-10-08 07:32:53 +0800

    fix: Add Neotree git icons

--- nvim/.config/nvim/lua/plugins/explorer.lua
+++ nvim/.config/nvim/lua/plugins/explorer.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    sources = { "filesystem", "buffers", "git_status" },
     filesystem = {
       filtered_items = {
         hide_dotfiles = false,
@@ -19,19 +20,33 @@ return {
         default = "",
         highlight = "NeoTreeFileIcon",
       },
+      git_status = {
+        symbols = {
+          added     = "✚",
+          modified  = "",
+          deleted   = "✖",
+          renamed   = "󰁕",
+          untracked = "",
+          ignored   = "",
+          unstaged  = "󰄱",
+          staged    = "",
+          conflict  = "",
+        },
+      },
     },
     renderers = {
       directory = {
         { "indent" },
         { "icon" },
-        { "name",  use_git_status_colors = false },
+        { "name",  use_git_status_colors = true },
       },
       file = {
         { "indent" },
-        { "name",  use_git_status_colors = false },
+        { "name",  use_git_status_colors = true },
+        { "git_status", highlight = "NeoTreeDimText" },
       },
     },
-    enable_git_status = false,
+    enable_git_status = true,
     enable_diagnostics = false,
   },
 }
