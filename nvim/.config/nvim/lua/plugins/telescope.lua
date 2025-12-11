56767ef5578af73d5813d0d7a8867495bc29d8b2
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-12-11 10:42:21 +0800

    fix: Make telescope full screen

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = {
+        width = 0.99,
+        height = 0.99,
+        preview_width = 0.6,
+      },
+    },
+  },
+}
+
